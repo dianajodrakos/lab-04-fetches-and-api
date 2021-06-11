8cf0eff9eca937354fafbb209c58b90563ecc0ab
DJ Drakos <dianajodrakos@gmail.com>
2021-06-11 07:32:53 -0700

    added search by options

--- src/App.js
+++ src/App.js
@@ -10,41 +10,64 @@ export default class App extends Component {
     loading: false,
     pokedex: [],
     query: '',
-    sortOrder: '',
+    queryCat: 'pokemon',
+    sortCat: 'pokemon',
+    sortOrder: 'asc',
   }
-
-  //display all pokemon on page load (mount)
-  componentDidMount = async () => {
+  
+  
+  
+  //make fetch function with loading toggle
+  fetchPokemon = async () => {
     //loading state true
     this.setState({ loading: true });
+
+    //make url ternary, account for sort option
+    const URL = this.state.query 
+    ? `https://pokedex-alchemy.herokuapp.com/api/pokedex?&pokemon=${this.state.query}&sort=${this.state.sortCat}&direction=${this.state.sortOrder}&perPage=50` 
+    : `https://pokedex-alchemy.herokuapp.com/api/pokedex?&sort=${this.state.sortCat}&direction=${this.state.sortOrder}&perPage=50`;
+
     //async function to get API
-    let pokedexAPI = await request.get('https://pokedex-alchemy.herokuapp.com/api/pokedex');
+    const pokedexAPI = await request.get(URL);
     
     //loading state false, set API request to state
     this.setState({ pokedex: pokedexAPI.body.results });
     this.setState({ loading: false });
   }
 
-  handleClick = (e) => {
-    console.log('click')
+
+  //display all pokemon on page load (mount)
+  componentDidMount = async () => {
+    this.fetchPokemon();
+  }
+
+  handleClick = () => {
+    this.fetchPokemon();
   }
 
   handleQueryChange = (e) => {
     this.setState({ query: e.target.value });
   }
 
+  handleQueryCatChange = (e) => {
+    this.setState({ queryCat: e.target.value });
+  }
+
   handleSortChange = (e) => {
     this.setState({ sortOrder: e.target.value });
+  }
+
+  handleSortCatChange = (e) => {
+    this.setState({ sortCat: e.target.value });
   }
 
   render() {
 
     return (
       <div>
-        <Header handleClick={this.handleClick} handleQueryChange={this.handleQueryChange} handleSortChange={this.handleSortChange} />
+        <Header handleClick={this.handleClick} handleQueryChange={this.handleQueryChange} handleQueryCatChange={this.handleQueryCatChange} handleSortChange={this.handleSortChange} handleSortCatChange={this.handleSortCatChange}/>
 
         { this.state.loading && <img src="./loadingpokedex.gif" alt="loading"/> }
-        { console.log(this.state.pokedex)}
         {/* display loading image until pokedex is ready to load */}
         <PokeList data={this.state.pokedex} />
       </div>
--- src/Header.js
+++ src/Header.js
@@ -5,12 +5,31 @@ export default class Header extends Component {
         return (
             <div>
                 this is my cool header
+                <select onChange={this.props.handleQueryCatChange}>
+                    <option disabled selected={true} value>Search Category:</option>
+                    <option value="pokemon">Pokemon</option>
+                    <option value="id">Species ID</option>
+                    <option value="attack">Attack</option>
+                    <option value="defense">Defense</option>
+                    <option value="type">Type</option>
+                </select>
                 <input type="text" placeholder="Pokemon name" onChange={this.props.handleQueryChange}/>
-                <button onClick={this.props.handleClick}>search</button>
+
+                <select onChange={this.props.handleSortCatChange}>
+                <option disabled selected={true} value>Sort Category:</option>
+                    <option value="pokemon">Pokemon</option>
+                    <option value="id">Species ID</option>
+                    <option value="attack">Attack</option>
+                    <option value="defense">Defense</option>
+                </select>
+
                 <select onChange={this.props.handleSortChange}>
+                    <option disabled selected={true} value>Sort by:</option>
                     <option value="asc">Ascending</option>
                     <option value="desc">Descending</option>
                 </select>
+
+                <button onClick={this.props.handleClick}>search</button>
             </div>
         )
     }
