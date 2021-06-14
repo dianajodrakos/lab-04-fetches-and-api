c4c55a3745a3fcd4ac151dd3789a4f7d77c24915
DJ Drakos <dianajodrakos@gmail.com>
2021-06-14 16:53:42 -0700

    set state to pokedex ID generated by matching params ID

--- src/PokeDetail.js
+++ src/PokeDetail.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import request from 'superagent';
 
 export default class PokeDetail extends Component {
+
+    state = {
+        pokemon: '',
+    }
+
+    //set state on component load
+    componentDidMount() {
+        this.fetchDetail();
+    }
+
+    //write fetch function to set state to match params ID
+    fetchDetail = async () => {
+        const pokeData = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex/${this.props.match.params.id}`);
+
+        this.setState({ pokemon: pokeData.body });
+    }
+
     render() {
+        console.log(this.props);
+        console.log(this.state);
         return (
-            <div>
-                
+            <div className="card">
+                <h2> {this.props.match.params.id} </h2>
+                <h2> {this.state.pokemon._id} </h2>
             </div>
         )
     }
--- src/PokeList.js
+++ src/PokeList.js
@@ -7,9 +7,9 @@ export default class PokeList extends Component {
     render() {
         return (
             <div className="list">
-                { this.props.data.map((pokemon, i) => 
-                <Link to="/pokemon/:id" data={pokemon} key={i}>
-                    <PokeItem data={pokemon} key={i}/>
+                { this.props.data.map((pokemon) => 
+                <Link to={`/pokemon/${pokemon._id}`} >
+                    <PokeItem data={pokemon} key={pokemon.id} />
                 </Link>
             )}
             </div>
